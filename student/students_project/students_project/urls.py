3caa190e43098bd4fd2ce09ee08522e88151e450
natali <natalia.tokhadze@gmail.com>
2024-10-31 17:59:35 +0400

    add: toolbar

--- student/students_project/students_project/urls.py
+++ student/students_project/students_project/urls.py
@@ -17,15 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from students import views
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', views.students_page, name='students'),
     path('main/', views.main_page, name='main')
-]
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('students/', include('students.urls')),
-#     path('main/', include('students.urls'))
-# ]
+] + debug_toolbar_urls()
